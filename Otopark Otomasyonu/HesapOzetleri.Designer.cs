53153a39aba5c02c1d91c5b1a682d800250abbf9
Can Ata Tekirdağlı <canata.tekirdagli@darussafaka.net>
2022-05-04 02:26:54 +0300

    asd

--- Otopark Otomasyonu/HesapOzetleri.Designer.cs
+++ Otopark Otomasyonu/HesapOzetleri.Designer.cs
@@ -32,8 +32,6 @@
             this.geri_don = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.park_yeri = new System.Windows.Forms.ComboBox();
-            this.label7 = new System.Windows.Forms.Label();
             this.groupBox3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
@@ -54,12 +52,10 @@
             // groupBox3
             // 
             this.groupBox3.BackColor = System.Drawing.Color.Navy;
-            this.groupBox3.Controls.Add(this.park_yeri);
-            this.groupBox3.Controls.Add(this.label7);
             this.groupBox3.Controls.Add(this.dataGridView1);
             this.groupBox3.Font = new System.Drawing.Font("Cascadia Mono", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.groupBox3.ForeColor = System.Drawing.SystemColors.Control;
-            this.groupBox3.Location = new System.Drawing.Point(51, 44);
+            this.groupBox3.Location = new System.Drawing.Point(60, 44);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(1196, 448);
             this.groupBox3.TabIndex = 11;
@@ -70,31 +66,12 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(6, 139);
+            this.dataGridView1.Location = new System.Drawing.Point(6, 26);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1184, 303);
+            this.dataGridView1.Size = new System.Drawing.Size(1184, 416);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // park_yeri
-            // 
-            this.park_yeri.Font = new System.Drawing.Font("Cascadia Mono", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.park_yeri.FormattingEnabled = true;
-            this.park_yeri.Location = new System.Drawing.Point(552, 43);
-            this.park_yeri.Name = "park_yeri";
-            this.park_yeri.Size = new System.Drawing.Size(292, 38);
-            this.park_yeri.TabIndex = 13;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Cascadia Mono", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.label7.Location = new System.Drawing.Point(195, 46);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(312, 30);
-            this.label7.TabIndex = 14;
-            this.label7.Text = "Gösterilecek Değerler :";
             // 
             // hesap_kesim
             // 
@@ -111,7 +88,6 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "HESAP ÖZETLERİ";
             this.groupBox3.ResumeLayout(false);
-            this.groupBox3.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
@@ -122,7 +98,5 @@
         private System.Windows.Forms.Button geri_don;
         private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.ComboBox park_yeri;
-        private System.Windows.Forms.Label label7;
     }
 }
