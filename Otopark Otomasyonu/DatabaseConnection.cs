3d69c4ee7f7fd783144ab5f09de07600f239e560
EMIN <EMIN@BABAYIGIT>
2022-05-05 03:53:00 +0300

    Otopark Durumu dışında exceldeki bütün maddeler bitti

--- Otopark Otomasyonu/DatabaseConnection.cs
+++ Otopark Otomasyonu/DatabaseConnection.cs
@@ -18,7 +18,7 @@ namespace Otopark_Otomasyonu
         SqlDataReader reader;
         public DatabaseConnection()
         {
-            connectionString = "Data Source=.\\SQLEXPRESS;Initial Catalog=otopark_simulasyonu;Integrated Security = True";
+            connectionString = "Data Source=CAN-ATA;Initial Catalog=otopark_simulasyonu;Integrated Security = True";
             connection = new SqlConnection(connectionString);
           
         }
